e6ddb71817393ae9f6f6dff6f8dfc4c35a69e119
panindresahil <46246306+panindresahil@users.noreply.github.com>
2020-06-07 14:59:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+function pro1()
+{
+    document.getElementById("product-img").innerHTML = '<img src="images/industrial_img_2.jpg" class="product-img" width="400px" height="400px">';
+    document.getElementById("product-info").innerHTML = '<img src="images/info1.PNG" class="product-info" width="400px" height="400px">'
+}
+function pro2()
+{
+    document.getElementById("product-img").innerHTML = '<img src="images/industrial_img_3.jpg" class="product-img" width="400px" height="400px">';
+    document.getElementById("product-info").innerHTML = '<img src="images/info2.PNG" class="product-info" width="400px" height="400px">'
+}
+function pro3()
+{
+    document.getElementById("product-img").innerHTML = '<img src="images/industrial_img_4.jpg" class="product-img" width="400px" height="400px">';
+    document.getElementById("product-info").innerHTML = '<img src="images/info3.PNG" class="product-info" width="400px" height="400px">'
+}
+function pro4()
+{
+    document.getElementById("product-img").innerHTML = '<img src="images/industrial_img_5.jpg" class="product-img" width="400px" height="400px">';
+    document.getElementById("product-info").innerHTML = '<img src="images/info4.PNG" class="product-info" width="400px" height="400px">'
+}
+function pro5()
+{
+    document.getElementById("product-img").innerHTML = '<img src="images/industrial_img_6.jpg" class="product-img" width="400px" height="400px">';
+    document.getElementById("product-info").innerHTML = '<img src="images/info5.PNG" class="product-info" width="400px" height="400px">'
+}
+
